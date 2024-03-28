f66ff81bd769bec4b06191606907a9b73597a84d
larissa-si-er <larissabrncreis@gmail.com>
2024-03-28 20:05:32 -0300

    dark mode

--- js/script.js
+++ js/script.js
@@ -14,3 +14,53 @@ body.addEventListener("click" , e => {
         nav.classList.remove("ativo");
     }
 });
+
+// acessibilidade
+const acess = document.querySelector('.acess-bt');
+const acess_Window = document.querySelector(".acess-window");
+
+acess.addEventListener ("click", () => {
+    if (acess_Window.style.display === "block") {
+        acess_Window.style.display = "none";
+      } else {
+        acess_Window.style.display = "block";
+      }
+})
+// acessibilidade - dk mode
+document.addEventListener('DOMContentLoaded', () => {
+    const darkModeStorage = localStorage.getItem('dark-mode')
+    const html = document.querySelector('html')
+    const header = document.querySelector('header')
+    const p = document.querySelector('p')
+    const h1 = document.querySelector('h1')
+    const h2 = document.querySelector('h2')
+    const h3 = document.querySelector('h3')
+    const h1_c3 = document.querySelector('.h1-c3')
+    const section = document.querySelector('section')
+    const footer = document.querySelector('footer')
+    const bt = document.querySelector('.b1-c1')
+    const inputDarkMode = document.querySelector('.input-dark-mode')
+
+    if(darkModeStorage){
+        html.setAttribute("black", "true");
+    }
+
+    inputDarkMode.addEventListener('change', () => {
+        if(inputDarkMode.checked){
+            html.setAttribute("dark", "true")
+            body.setAttribute("dark", "true")
+            localStorage.setItem('dark-mode', true);
+
+
+        }else{
+            html.removeAttribute("dark")
+            localStorage.removeItem('dark-mode');
+            body.removeAttribute("dark")
+
+
+        }
+   })
+})
+
+// DARK MODE - FIM
+
